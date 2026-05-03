01ceb2eec9b9241c521e170a338dcc7db084d89a
Jimmy <bjorken93@hotmail.se>
2026-05-03 11:14:59 +0200

    added video_stats.py

---
+++ video_stats.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+API_KEY = "AIzaSyCANPh8UqMwL8y9-FWa9c4C_cdrbsauZmk"
+
+CHANNEL_HANDLE = "MrBeast"
+
+def get_playlist_id():
+        
+    try:
+
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        # print(json.dumps(data,indent=4))
+
+
+        channel_items = data["items"][0]
+
+        channel_playlist_id = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+
+
+        # print(channel_playlist_id)
+
+        return channel_playlist_id
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+if __name__ == "__main__":
+    get_playlist_id()
