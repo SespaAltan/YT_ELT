7285526a3179d697020c6fac95b99a47ceacd6bc
Jimmy <bjorken93@hotmail.se>
2026-05-10 22:02:06 +0200

    fixat DAGsen så att den laddar med beroenden av varandra så att dom går av i rätt ordningen och enbart ifall tidigare ahar lyckats

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
 
 from datawarehouse.dwh import staging_table, core_table
@@ -34,7 +35,7 @@ with DAG(
     description='DAG to produce JSON file with raw data',
     schedule='0 14 * * *',
     catchup=False
-) as dag:
+) as dag_produce:
     
     # Define tasks
     playlist_id = get_playlist_id()
@@ -42,30 +43,39 @@ with DAG(
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
     # Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
 with DAG(
     dag_id='update_db',
     default_args=default_args,
     description='DAG to process JSON file and insert data into both staging and core schemas',
-    schedule='0 15 * * *',
-    catchup=False
+    catchup=False,
+    schedule=None,
 ) as dag_update:
     
     # Define tasks
     update_staging = staging_table()
     update_core = core_table()
 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
+
     # Define dependencies
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
  
 with DAG(
     dag_id='data_quality',
     default_args=default_args,
     description='DAG to check the data quality on both layers in the db',
-    schedule='0 16 * * *',
-    catchup=False
+    catchup=False,
+    schedule=None,
 ) as dag_quality:
     
     # Define tasks
